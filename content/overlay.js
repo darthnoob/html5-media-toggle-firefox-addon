2329f9cb751f166124c25e8390582ce8ce16d57b
spenibus <git@spenibus.net>
2014-10-11 16:43:55 +0200

    added autoplay toggle

--- content/overlay.js
+++ content/overlay.js
@@ -1,25 +1,19 @@
-/*******************************************************************************
-toggle html5 media support
-
-creation: 2010-03-09 00:00 - spenibus
-  update: 2014-10-09 21:13 - spenibus
-*******************************************************************************/
-
-
-
-
 /******************************************************************************/
 var spenibus_html5MediaToggle = {
 
 
-   /****************************************************************** button */
-   button: null,
+   /***************************************************************** buttons */
+   button_id_support:  'spenibus_html5_media_toggle_button_support',
+   button_id_autoplay: 'spenibus_html5_media_toggle_button_autoplay',
+   button_support:  null,
+   button_autoplay: null,
 
 
 
 
-   /************************************************************** prefs list */
-   prefs : [
+   /******************************************************************* prefs */
+   prefs_autoplay : 'media.autoplay.enabled',
+   prefs_support : [
       'media.directshow.enabled', // 0:reference
       'media.windows-media-foundation.enabled',
       'media.ogg.enabled',
@@ -33,10 +27,16 @@ var spenibus_html5MediaToggle = {
 
 
 
+   /*********************************************************** prefs service */
+   ps : null,
+
+
+
+
    /**************************************************************** observer */
    init : function() {
 
-      // self
+      // self reference for out of context calls
       var me = this;
 
       // get prefs service root branch
@@ -44,23 +44,20 @@ var spenibus_html5MediaToggle = {
          .getService(Components.interfaces.nsIPrefService);
 
       // add observer
-      this.ps.addObserver("", spenibus_html5MediaToggle, false);
+      this.ps.addObserver('', spenibus_html5MediaToggle, false);
 
-      // get button element, try document first
-      this.button = document.getElementById('spenibus_html5_media_toggle_button');
+      // get buttons element, try document first
+      this.button_support  = this.buttonGet(this.button_id_support);
+      this.button_autoplay = this.buttonGet(this.button_id_autoplay);
 
-      // try toolbar palette if document yielded nothing
-      if(this.button == null) {
-         for(var i = 0; i < gNavToolbox.palette.childNodes.length; ++i) {
-            if(gNavToolbox.palette.childNodes[i].id == 'spenibus_html5_media_toggle_button') {
-               this.button = gNavToolbox.palette.childNodes[i];
-            }
-         }
-      }
+      // add command to button: support
+      this.button_support.addEventListener("command", function(){
+         me.toggleSupport.call(me);
+      }, false);
 
-      // add command to button
-      this.button.addEventListener("command", function(){
-         me.toggle.call(me)
+      // add command to button: autoplay
+      this.button_autoplay.addEventListener("command", function(){
+         me.toggleAutoplay.call(me);
       }, false);
 
       // update button
@@ -73,10 +70,34 @@ var spenibus_html5MediaToggle = {
    /**************************************************************** observer */
    observe : function(subject, topic, data) {
 
+      var t = topic == "nsPref:changed";
+      var d = data == this.prefs_support[0] || data == this.prefs_autoplay;
+
       // reference pref has changed, update button
-      if(topic == "nsPref:changed" && data == this.prefs[0]) {
+      if(t && d) {
          this.buttonUpdate();
       }
+   },
+
+
+
+
+   /*********************************************************** button getter */
+   buttonGet : function(id) {
+
+      // try document first
+      var b = document.getElementById(id);
+
+      // try toolbar palette if document yielded nothing
+      if(b == null) {
+         for(var i=0; i<gNavToolbox.palette.childNodes.length; ++i) {
+            if(gNavToolbox.palette.childNodes[i].id == id) {
+               b = gNavToolbox.palette.childNodes[i];
+            }
+         }
+      }
+
+      return b;
    },
 
 
@@ -85,27 +106,43 @@ var spenibus_html5MediaToggle = {
    /********************************************************** button updater */
    buttonUpdate : function() {
 
-      this.button.setAttribute(
+      // support
+      this.button_support.setAttribute(
          'data-enabled',
-         this.ps.getBoolPref(this.prefs[0]) ? 'true' : 'false'
+         this.ps.getBoolPref(this.prefs_support[0]) ? 'true' : 'false'
+      );
+
+      // autoplay
+      this.button_autoplay.setAttribute(
+         'data-enabled',
+         this.ps.getBoolPref(this.prefs_autoplay) ? 'true' : 'false'
       );
    },
 
 
 
 
-   /***************************************************************** toggler */
-   toggle : function() {
+   /********************************************************* support toggler */
+   toggleSupport : function() {
 
       // get reversed reference value
-      var enabled = !this.ps.getBoolPref(this.prefs[0]);
+      var enabled = !this.ps.getBoolPref(this.prefs_support[0]);
 
       // set all prefs
-      for(var i=0; i<this.prefs.length; ++i) {
-         this.ps.setBoolPref(this.prefs[i], enabled);
+      for(var i=0; i<this.prefs_support.length; ++i) {
+         this.ps.setBoolPref(this.prefs_support[i], enabled);
       }
    },
 
+
+
+
+   /******************************************************** autoplay toggler */
+   toggleAutoplay : function() {
+
+      // set reverse pref
+      this.ps.setBoolPref(this.prefs_autoplay, !this.ps.getBoolPref(this.prefs_autoplay));
+   },
 
 };
 
@@ -114,6 +151,5 @@ var spenibus_html5MediaToggle = {
 
 /*********************************************************************** init */
 window.addEventListener("load", function(){
-   var me = spenibus_html5MediaToggle;
-   me.init.call(me);
+   spenibus_html5MediaToggle.init.call(spenibus_html5MediaToggle);
 }, false);
---
+++ readme.txt
@@ -0,0 +1,3 @@
+html5 media toggle
+
+This is a firefox addon that allows the user to toggle html5 media support and autoplay.
